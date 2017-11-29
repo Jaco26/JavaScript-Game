5650277851d8ffd803e5ced0bf6ad5f391e0ec2f
Jaco26 <32335624+Jaco26@users.noreply.github.com>
2017-11-28 21:13:07 -0600

    nothing super special

--- main.js
+++ main.js
@@ -5,21 +5,26 @@
 //Create empty array "ranArray[]"
 //Use for loop to run a code block the same number of times as the number of elements in 
 //   the built-in "forms[]" array (16 times because there are 16 elements in the <form name"gameBoard">)
-//In the code block, I 1: declared a variable equal to the number of elements inside the <form name"gameBoard">
-//  then 2:
+//In the code block, I 1: declared a variable "nOfInputs" equal to the number of elements inside the <form name"gameBoard">
+//  then 2: declared a variable random 
 
 
-var ranArray = [];
+
+var ranArray = new Array();
 
 function ranButton(){
 	for(i = 0; i < document.gameBoard.length; i++){
 		var nOfInputs = document.gameBoard.length;
 		var randomButton = document.gameBoard.elements[Math.floor(Math.random()*nOfInputs)];
-		var newItems = ranArray.push(randomButton);
-
+		var newItems = ranArray.push(randomButton.value);
 		alert(randomButton.id);
 	}
 }
 
+
+
+
+var pair1 = ranArray[0];
+console.log(pair1);
 
 
