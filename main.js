90ec9a10a64c2ab9429f12ed22a4bf4d12589486
Jaco26 <32335624+Jaco26@users.noreply.github.com>
2017-11-28 20:22:06 -0600

    FUCK YES!!
    I modified the code block in the loop in ranButton() to add to a new array---resulting in a randomized array of <input> elements

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 
-//randomly select two <input> elements from inside <form name="gameBoard">
+//randomly select all <input> elements from inside <form name="gameBoard">
+//Also, create a new array which holds the original 
+
+//Create empty array "ranArray[]"
+//Use for loop to run a code block the same number of times as the number of elements in 
+//   the built-in "forms[]" array (16 times because there are 16 elements in the <form name"gameBoard">)
+//In the code block, I 1: declared a variable equal to the number of elements inside the <form name"gameBoard">
+//  then 2:
+
+
+var ranArray = [];
+
 function ranButton(){
-	for(i = 0; i < 2; i++){
+	for(i = 0; i < document.gameBoard.length; i++){
 		var nOfInputs = document.gameBoard.length;
 		var randomButton = document.gameBoard.elements[Math.floor(Math.random()*nOfInputs)];
+		var newItems = ranArray.push(randomButton);
+
 		alert(randomButton.id);
 	}
 }
-
-
-
 
 
 
