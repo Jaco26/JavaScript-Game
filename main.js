ce275a74dc7c398fa50ff590683d6d0786baae89
Jaco26 <32335624+Jaco26@users.noreply.github.com>
2017-11-28 18:57:10 -0600

    Simplified JS
    JS: now only use ranButton() to randomly select two <input>s from the <form>
    
    HTML: removed unnecessary buttons

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
 
-//randomly select an <input> element inside <form name="gameBoard">
-function randomSquare(){
-		var formLength = document.gameBoard.length;
-		var x = document.gameBoard.elements[Math.floor(Math.random()*formLength)];
-		return x.id;
-}
-
-//pops-up the returned value (x.id) from randomSquare() to make sure it's returning a random value
-function button(){
-	alert(randomSquare());
-}
-
-
-function randomTwo(){
-	for (i=0; i<2; i++){
-	alert(randomSquare());
+//randomly select two <input> elements from inside <form name="gameBoard">
+function ranButton(){
+	for(i = 0; i < 2; i++){
+		var nOfInputs = document.gameBoard.length;
+		var randomButton = document.gameBoard.elements[Math.floor(Math.random()*nOfInputs)];
+		alert(randomButton.id);
 	}
 }
+
+
+
+
+
+
